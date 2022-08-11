56d18757052b62f1552754c77412f60fda369980
Amyas Merivale <merivale@gmail.com>
2022-08-11 18:38:44 +0200

    Update deps

--- deps.ts
+++ deps.ts
@@ -1,7 +1,14 @@
-export { type Cookie, deleteCookie, getCookies, setCookie, serve, Status } from "https://deno.land/std@0.142.0/http/mod.ts"
+export {
+  type Cookie,
+  deleteCookie,
+  getCookies,
+  setCookie,
+  serve,
+  Status,
+} from "https://deno.land/std@0.151.0/http/mod.ts"
 
-export { contentType } from "https://deno.land/std@0.142.0/media_types/mod.ts"
+export { contentType } from "https://deno.land/std@0.151.0/media_types/mod.ts"
 
-export { dirname, extname } from "https://deno.land/std@0.142.0/path/mod.ts"
+export { dirname, extname } from "https://deno.land/std@0.151.0/path/mod.ts"
 
-export { build } from "https://deno.land/x/esbuild@v0.14.45/mod.js"
+export { build } from "https://deno.land/x/esbuild@v0.15.1/mod.js"
